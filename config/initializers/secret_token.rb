db359ce41b7a0498d7dbdf5fe76d93103b559547
Erin <zazainca@gmail.com>
2013-08-14 10:02:47 -0700

    First commit and landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaylasPage::Application.config.secret_key_base = '88b70865aba59a2b63b288f204771c4b6ded6668df3b436e89a5301fa74b0a07cd676feb317a5dcad868ef05254974d6ce9a1d016bce3a9a6c477f0ddb19b6d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaylasPage::Application.config.session_store :cookie_store, key: '_LaylasPage_session'
